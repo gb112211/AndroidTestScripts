85e9d753c29474a9532334d28b1c437a48823009
xuxu <xuxu_1988@126.com>
2015-03-11 18:12:05 +0800

    modified kill_5037.py

--- python/kill_5037.py
+++ python/kill_5037.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 '''
@@ -8,22 +7,23 @@ Created on 2015年2月28日
 '''
 
 import os
+import platform
 
-from scriptUtils import utils
-
-#5037端口占用时杀掉占用该端口的进程
-
-def linux():
-    pid = os.popen("netstat -anop | grep 5037 | grep  LISTEN").read().split()[6].split("/")[0]
-    os.system("kill %s" %pid)
+#5037端口占用时,打开该进程路径，且杀掉占用该端口的进程（只支持Windows）
 
 def win():
     pid = os.popen("netstat -ano | findstr 5037 | findstr  LISTENING").read().split()[-1]
+    process_name = os.popen('tasklist /FI "PID eq %s"' %pid).read().split()[-6]
+    process_path = os.popen('wmic process where name="%s" get executablepath' %process_name).read().split()[-1]
+    #分割路径，得到进程所在文件夹名
+    name_list = process_path.split("\\")
+    del name_list[-1]
+    directory = "\\".join(name_list)
+    #打开进程所在文件夹
+    os.system("explorer.exe %s" %directory)
+    #杀死该进程
     os.system("taskkill /F /PID %s" %pid)
     
 
 if __name__ == "__main__":
-    if utils.system is "Windows":
-        win()
-    else:
-        linux()
+    win()
