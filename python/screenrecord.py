81af404ec2249eb2fa0c0123e59f78161256b4f2
rikixu(胥敏) <rikixu@tencent.com>
2018-05-01 15:04:33 +0800

    modify screenrecord.py

--- python/screenrecord.py
+++ python/screenrecord.py
@@ -14,26 +14,25 @@ import time
 
 from scriptUtils import utils
 
-#需要Android4.4及4.4以上版本，运行脚本后可录制设备上的操作，默认使用手机分辨率，时间3min。手动按Enter结束录制。
+#需要Android4.4及4.4以上版本，运行脚本后可录制设备上的操作，默认使用手机分辨率，手动设置录制时间。
 #录制结果存放于当前目录下的video目录下
 
 PATH = lambda p: os.path.abspath(p)
 
 def record():
-    utils.shell("screenrecord /data/local/tmp/video.mp4")  
-    input_key = raw_input("Please press the Enter key to stop recording:\n")
-    if input_key == "":
-        utils.adb("kill-server")
-    
-    print "Get Video file..."
-    utils.adb("start-server")
-    time.sleep(1.5)
-    
-    path = PATH("%s/video" %os.getcwd())
-    if not os.path.isdir(path):
-        os.makedirs(path)
-    
-    utils.adb("pull /data/local/tmp/video.mp4 %s"  %PATH("%s/%s.mp4" %(path, utils.timestamp()))).wait()
+    utils.shell("rm -f /data/local/tmp/video.mp4")
+    limit_time = raw_input("Please set the maximum recording time, in seconds.  Maximum is 180.\n")
+    if limit_time == "":
+        utils.shell("screenrecord --time-limit 180 /data/local/tmp/video.mp4")
+    try:
+        _limit_time = int(limit_time) + 1
+    except:
+        record()
+    if 0 < _limit_time <= 180:
+        utils.shell("screenrecord --time-limit %s /data/local/tmp/video.mp4" %limit_time).wait()
+    else:
+        print "Please set again!"
+        record()
         
 if __name__ == "__main__":
     sdk = string.atoi(utils.shell("getprop ro.build.version.sdk").stdout.read())
@@ -42,4 +41,12 @@ if __name__ == "__main__":
         sys.exit(0)
     else:
         record()
+        print "Get Video file..."
+        time.sleep(3)
+
+        path = PATH("%s/video" %os.getcwd())
+        if not os.path.isdir(path):
+            os.makedirs(path)
+
+        os.popen("adb pull /data/local/tmp/video.mp4 %s"  %PATH("%s/%s.mp4" %(path, utils.timestamp())))
         print "Completed"
